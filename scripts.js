b270f03f3dcf59e457f4bad87c8e977b5e31f275
Alice Chuang <bomberetta@gmail.com>
2017-12-31 13:22:56 -0500

    template

--- scripts.js
+++ scripts.js
@@ -1,13 +1,75 @@
+// from githubuniverse.com
+
 var ENGAGE = ENGAGE || {};
 
 (
   function($, APP) {
       $(function() {
+          APP.Viewport.init();
           APP.Background.init();
+          // console.log(Object.getOwnPropertyNames(APP));
       });
+
+      APP.Viewport = {
+          init: function() {
+              $.fn.isOnScreen = function(){
+                  var elementTop = $(this).offset().top,
+                    elementBottom = elementTop + $(this).outerHeight(),
+                    viewportTop = $(window).scrollTop(),
+                    viewportBottom = viewportTop + $(window).height();
+                  return elementBottom > viewportTop && elementTop < viewportBottom;
+              };
+
+              function detection() {
+                  for(var i = 0; i < items.length; i++) {
+                      var el = $( items[i] );
+
+                      if( el.isOnScreen() ){
+                          el.addClass('in-view');
+                      } else {
+                          el.removeClass('in-view');
+                      }
+                  }
+              }
+
+              var items = document.querySelectorAll('*[data-animate-in], *[data-detect-viewport]'),
+                waiting = false,
+                w = $(window);
+
+              w.on("resize scroll", function(){
+                  if (waiting) {
+                      return;
+                  }
+                  waiting = true;
+                  detection();
+
+                  setTimeout(function () {
+                      waiting = false;
+                  }, 100);
+              });
+
+              $(document).ready(function(){
+                  setTimeout(function(){
+                      detection();
+                  }, 500);
+
+                  for(var i = 0; i < items.length; i++) {
+                      var d = 0,
+                        el = $( items[i] );
+                      if( items[i].getAttribute('data-animate-in-delay') ) {
+                          d = items[i].getAttribute('data-animate-in-delay') / 1000 + 's';
+                      } else {
+                          d = 0;
+                      }
+                      el.css('transition-delay', d);
+                  }
+              });
+          }
+      };
 
       APP.Background = {
           init: function() {
+              console.log('test',$('.gradient, .shooting-star, .stars').length);
               if( $('.gradient, .shooting-star, .stars').length ) {
                   this.stars();
               } else {
