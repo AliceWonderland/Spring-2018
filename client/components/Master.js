218daf7d6164646e8b59aefd0340c11a2e4776e9
Alice Chuang <bomberetta@gmail.com>
2018-01-29 01:49:56 -0500

    master

--- client/components/Master.js
+++ client/components/Master.js
@@ -28,7 +28,7 @@ export default class Master extends Component{
                       <span data-animate-in="intro"><a href="#">SECTION 1 TEXT</a></span>
 
                       <div data-animate-in="intro">Lorem <a href="#">ipsum</a> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-                      <div data-animate-in="intro"><Copy/></div>
+                      <div data-animate-in="intro">Copy</div>
                   </div>
 
                   <figure className="shooting-star" style={{top: '8%', transform: 'rotate(151deg)'}}>
