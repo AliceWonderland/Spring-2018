9d5bf8c6a92d4270c4d1d4c5820c9082cd555200
Alice Chuang <bomberetta@gmail.com>
2018-01-29 01:44:33 -0500

    master

--- client/components/Master.js
+++ client/components/Master.js
@@ -42,15 +42,15 @@ export default class Master extends Component{
               </div>
 
               <div className="horizon" data-animate-in="intro" style={{height: '400px'}}>
-                  blah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blahblah blah blah
+                  Lorem <a href="#">ipsum</a> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                   <div className="stars"></div>
                   <div className="stars-lg"></div>
               </div>
 
               <div className="space">
                   <div data-animate-in="intro">Lorem <a href="#">ipsum</a> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-                  <div data-animate-in="intro">blab</div>
-                  <div data-animate-in="intro">blab</div>
+                  <div data-animate-in="intro">Copy</div>
+                  <div data-animate-in="intro">Copy</div>
               </div>
           </main>
         )
